30acdc8aaab152fc0b5dedaa68b4a2cb333d10ae
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-09-12 20:50:14 +0600

    Create Take_input.java

---
+++ PrimaryJava/Take_input.java
@@ -0,0 +1,16 @@
+//assignment 2,4
+package com.mycompany.primaryjava;
+import java.util.Scanner;
+
+public class Product {
+    public static void main(String args[]){
+        int id;
+        String price;
+        Scanner i = new Scanner(System.in);
+        Scanner pr = new Scanner(System.in);
+        id = i.nextInt();
+        price = pr.next();
+        System.out.println("id : " + id);
+        System.out.println("Price : " + price);
+    }
+}
