161f5c7145094e11c49929b34c03b971aaeabbef
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-09-12 20:45:01 +0600

    Create Variable1.java

---
+++ PrimaryJava/Variable1.java
@@ -0,0 +1,19 @@
+//assignment 2,4
+package com.mycompany.primaryjava;
+
+
+public class Variable1 {
+    public static void main(String args[]){
+        int id = 103;
+        String title,cat,des,price;
+        title = "iphone15";
+        price = "105000";
+        cat = "phone";
+        des ="perfect product with best quality";
+        System.out.println("id : " + id);
+        System.out.println("title : " + title);
+        System.out.println("Price : " + price);
+        System.out.println("catagory : " + cat);
+        System.out.println("description : " + des);
+    }
+}
