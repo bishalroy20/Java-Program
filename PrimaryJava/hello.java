fbeb71bd8a6ef49509322976d554e91ac96df794
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-09-12 20:42:11 +0600

    Create hello.java

---
+++ PrimaryJava/hello.java
@@ -0,0 +1,9 @@
+package com.mycompany.primaryjava;
+// import java.util.Scanner;
+public class PrimaryJava {
+
+    public static void main(String[] args) {
+       System.out.println("Hello World!");
+
+    }
+}
