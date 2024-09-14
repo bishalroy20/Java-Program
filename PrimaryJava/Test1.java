581745a4db2edd43a68f12721874145ef8077243
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-09-14 20:59:07 +0600

    Create Test1.java

---
+++ PrimaryJava/Test1.java
@@ -0,0 +1,37 @@
+
+
+// package oop;
+
+// public class Teacher {
+//     String name,age;
+//     int phn;
+    
+//     //method build
+//     void display(){
+//         System.out.println("Name : "+name);
+//         System.out.println("Age : "+age);
+//         System.out.println("Phn : "+phn);
+//     }
+// }
+
+
+package oop;
+
+public class Test {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher();
+        Teacher t2 = new Teacher();
+        
+        t1.name = "Bishal Roy";
+        t1.age = "21";
+        t1.phn = 1309482127;
+        
+        t2.name = "Pushpita Sikder";
+        t2.age = "21";
+        t2.phn = 1876376522;
+        
+        t1.display();
+        t2.display();
+        
+    }
+}
