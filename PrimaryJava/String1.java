3b0312c20ffe1d2aeb480fbdd1a23a41eada09b7
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-09-14 12:54:40 +0600

    Create String1.java

---
+++ PrimaryJava/String1.java
@@ -0,0 +1,35 @@
+package Array_String;
+//import java.util.*;
+
+public class String1 {
+    public static void main(String[] args) {
+        String s1 = "Bishal Roy ";
+        String s2 = "Pushpita Sikder";
+        
+//        System.out.println("Length : " + s1.length());
+//        
+//        if(s1.equals(s2)){
+//            System.out.println("Equal");
+//        }
+//        else{
+//            System.out.println("Not Equal");
+//        }
+//        
+//        System.out.println("s2 contains 'Sikder' : "+s2.contains("Sikder"));
+//        
+//        System.out.println("String Empty : "+ s1.isEmpty());
+        
+        s1 = s1.toUpperCase();
+        s2 = s2.toUpperCase();
+        System.out.println(s1);
+        System.out.println(s2);
+//        System.out.println("Contain Sikder : " s2.contains());
+        
+        s1 = s1.concat(s2);
+        System.out.println("After concat : "+s1);
+        
+        
+        System.out.println("start Pu s1 : "+s1.startsWith("Bishal"));
+        System.out.println("end with oy s2 : "+s2.endsWith("Sikder"));
+    }
+}
