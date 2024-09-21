d1666bbf1d7cfe39096dd8e014b016052bae0349
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-09-21 10:47:22 +0600

    Create Static2.java

---
+++ PrimaryJava/Static2.java
@@ -0,0 +1,22 @@
+//                    static variable //
+
+
+// package staticVariable;
+
+
+// public class StaticVariable {
+//     static String uni = "Sylhet Engineering College";
+    
+// }
+
+
+package staticVariable;
+
+public class Testsv {
+    public static void main(String[] args) {
+//        StaticVariable ob = new StaticVariable(); // static variable obj diye access hoy na .
+
+        //eta class er sathe related , tai className.varianleName diye call korte hoy
+        System.out.println("Name : "+StaticVariable.uni);
+    }
+}
